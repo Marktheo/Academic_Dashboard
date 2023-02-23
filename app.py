7722684d20edc632c692f8f7b3d31a510c2b44ff
Marktheo <marcosvinytheo@gmail.com>
2023-02-23 15:09:47 -0300

    Application Updade: Academic History

--- app.py
+++ app.py
@@ -26,3 +26,34 @@ with column1:
 
 # Section Break
 st.markdown('***')
+
+
+# ---- Academic History ---- #
+# Column Configuration
+column2, column3 = st.columns([6, 2])
+
+# History DataSet
+hist = [['2022', 1, 1, 'Cálculo I', 'MAC118', 6, 6.4, 'Approved'],
+       ['2022', 1, 1, 'Algoritmos e Programação', 'COS110', 5, 10.0, 'Approved'],
+       ['2022', 1, 1, 'Lógica Matemática', 'COS230', 5, 9.4, 'Approved'],
+       ['2022', 1, 1, 'Química', 'IQG111', 4, 8.0, 'Approved'],
+       ['2022', 1, 1, 'Física I', 'FIT112', 4, 5.2, 'Approved'],
+       ['2022', 1, 1, 'Introdução à Eng. Controle', 'COE100', 2, 10.0, 'Approved'],
+       ['2022', 1, 1, 'Física Experimental I', 'FIS112', 1, 5.0, 'Approved'],
+       ['2022', 2, 2, 'Linguagens de Programação', 'EEL670', 5, 10.0, 'Approved'],
+       ['2022', 2, 2, 'Circuitos Lógicos', 'EEL280', 5, 7.7, 'Approved'],
+       ['2022', 2, 2, 'Economia A', 'EEI312', 4, 10.0, 'Approved'],
+       ['2022', 2, 2, 'Sistemas Projetivos', 'EEG110', 4, 8.3, 'Approved'],
+       ['2022', 2, 2, 'Física Experimental II', 'FIS121', 1, 7.7, 'Approved']]
+
+# History DataFrame Configuration
+history = pd.DataFrame(hist, columns=['Year', 'Semester', 'Period', 'Subject', 'Code', 'Credits', 'Grade', 'Situation'])
+
+# History Insert - Grade x Credits
+history.insert(8, 'Accum. Grade', history['Grade'] * history['Credits'])
+
+# Left Column
+with column2:
+    st.dataframe(data=history, use_container_width=True, height=458)
+
+

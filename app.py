e088854278d9c4f9c05db7f64a7e6e602a2a4bf2
Marktheo <marcosvinytheo@gmail.com>
2023-02-23 15:08:00 -0300

    Application Updade: Page Configuration

--- app.py
+++ app.py
@@ -3,3 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sb
 import pandas as pd
 
+
+# ---- Page Configuration ---- #
+st.set_page_config(page_title='Interactive Dashboard', layout='wide')
+

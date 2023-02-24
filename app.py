306ec69df146d543041fee0c39734316a2816024
Marktheo <marcosvinytheo@gmail.com>
2023-02-24 01:39:32 -0300

    Application Updade: High School Analysis

--- app.py
+++ app.py
@@ -15,71 +15,121 @@ column0, column1 = st.columns([3, 2])
 # Left Column
 with column0:
     st.title('Marcos Vinícius Theodoro Pinheiro da Silva')
-    st.subheader('Eletronics and Hardware Coordinator - UFRJ Nautilus')
+    st.subheader('Electronics and Hardware Coordinator - UFRJ Nautilus')
     st.subheader('Control and Automation Engeneering - UFRJ')
     st.subheader('Industrial Automation Technician - CEFET/RJ')
     st.caption('Since 2019, studying and developing projects related to robotics, control and automation.')
 
 # Right Column
 with column1:
-    st.image('Data.gif')
+    st.image('network.gif')
 
-# Section Break
+
+# ---- Section Break ---- #
 st.markdown('***')
 
 
-# ---- Academic History ---- #
-# Column Configuration
-column2, column3 = st.columns([6, 2])
+# ---- Column Configuration ---- #
+column2, column3 = st.columns([1, 1])
 
-# History DataSet
-hist = [['2022', 1, 1, 'Cálculo I', 'MAC118', 6, 6.4, 'Approved'],
-       ['2022', 1, 1, 'Algoritmos e Programação', 'COS110', 5, 10.0, 'Approved'],
-       ['2022', 1, 1, 'Lógica Matemática', 'COS230', 5, 9.4, 'Approved'],
-       ['2022', 1, 1, 'Química', 'IQG111', 4, 8.0, 'Approved'],
-       ['2022', 1, 1, 'Física I', 'FIT112', 4, 5.2, 'Approved'],
-       ['2022', 1, 1, 'Introdução à Eng. Controle', 'COE100', 2, 10.0, 'Approved'],
-       ['2022', 1, 1, 'Física Experimental I', 'FIS112', 1, 5.0, 'Approved'],
-       ['2022', 2, 2, 'Linguagens de Programação', 'EEL670', 5, 10.0, 'Approved'],
-       ['2022', 2, 2, 'Circuitos Lógicos', 'EEL280', 5, 7.7, 'Approved'],
-       ['2022', 2, 2, 'Economia A', 'EEI312', 4, 10.0, 'Approved'],
-       ['2022', 2, 2, 'Sistemas Projetivos', 'EEG110', 4, 8.3, 'Approved'],
-       ['2022', 2, 2, 'Física Experimental II', 'FIS121', 1, 7.7, 'Approved']]
 
-# History DataFrame Configuration
-history = pd.DataFrame(hist, columns=['Year', 'Semester', 'Period', 'Subject', 'Code', 'Credits', 'Grade', 'Situation'])
+# ---- High School History ---- #
+# History DataFrame Import
+history1 = pd.read_json('highHistory.json', orient ='records')
+
+# History DataFrame Format
+history1['Year'] = history1['Year'].astype(str)
 
 # History Insert - Grade x Credits
-history.insert(8, 'Accum. Grade', history['Grade'] * history['Credits'])
+history1.insert(6, 'Accum. Grade', history1['Grade'] * history1['Credits'])
 
 # Left Column
 with column2:
-    st.dataframe(data=history, use_container_width=True, height=458)
+    st.markdown('<h3 style="text-align: center">Technical High School History</h3><br>', unsafe_allow_html=True)
+    st.dataframe(data=history1.iloc[:, 0:6], use_container_width=True, height=1578)
 
 
-# ---- Performance Coefficient ---- #
+# ---- Graduation History ---- #
+# History DataFrame Import
+history0 = pd.read_json('gradHistory.json', orient ='records')
 
-# Coefficient - 1st Period
-P1 = round(history.loc[history['Period'] == 1].iloc[:, 8].sum() / history.loc[history['Period'] == 1].iloc[:, 5].sum(), 1)
+# History DataFrame Format
+history0['Year'] = history0['Year'].astype(str)
 
-# Coefficient - 2nd Period
-P2 = round(history.loc[history['Period'] <= 2].iloc[:, 8].sum() / history.loc[history['Period'] <= 2].iloc[:, 5].sum(), 1)
-
-# Coefficient DataSet
-coef = [[1, P1],
-       [2, P2]]
-
-# Coefficient DataFrame
-coefficient = pd.DataFrame(coef, columns=['Period', 'Performance Coefficient'])
-
-# Coefficient Histplot
-sb.set_style('whitegrid')
-plt.figure(figsize=(6,6))
-plt0 = sb.barplot(x='Period', y='Performance Coefficient', data=coefficient, palette='mako')
-plt0.set_title('Performance Coefficient')
-plt0.bar_label(plt0.containers[0])
-plt0.set_ylabel(None)
+# History Insert - Grade x Credits
+history0.insert(8, 'Accum. Grade', history0['Grade'] * history0['Credits'])
 
 # Right Column
 with column3:
+    st.markdown('<h3 style="text-align: center">Graduation History</h3><br>', unsafe_allow_html=True)
+    st.dataframe(data=history0.iloc[:, 0:8], use_container_width=True, height=458)
+
+
+# ---- Section Break ---- #
+st.markdown('***')
+
+
+# ---- Column Configuration ---- #
+column4, column5 = st.columns([1, 1])
+
+
+# ---- High School Performance Coefficient ---- #
+# Coefficient - 1st Year
+Y1 = round(history1.loc[history1['Year'] == "2019"].iloc[:, 6].sum() / history1.loc[history1['Year'] == "2019"].iloc[:, 3].sum(), 1)
+
+# Coefficient - 2nd Year
+Y2 = round(history1.loc[history1['Year'] == "2020"].iloc[:, 6].sum() / history1.loc[history1['Year'] == "2020"].iloc[:, 3].sum(), 1)
+
+# Coefficient - 3rd Year
+Y3 = round(history1.loc[history1['Year'] == "2021"].iloc[:, 6].sum() / history1.loc[history1['Year'] == "2021"].iloc[:, 3].sum(), 1)
+
+# Coefficient DataSet
+coef1 = [[2019, Y1],
+        [2020, Y2],
+        [2021, Y3]]
+
+# Coefficient DataFrame
+coefficient1 = pd.DataFrame(coef1, columns=['Year', 'Performance Coefficient'])
+
+# Coefficient Histplot
+sb.set_style('whitegrid')
+plt.figure(figsize=(10, 5))
+plt1 = sb.barplot(x='Year', y='Performance Coefficient', data=coefficient1, palette='mako')
+plt1.bar_label(plt1.containers[0])
+plt1.set(yticklabels=[])
+plt1.set_ylabel(None)
+plt1.set_xlabel(None)
+
+# Left Column
+with column4:
+    st.markdown('<h3 style="text-align: center">Technical High School Performance Coefficient</h3><br>', unsafe_allow_html=True)
+    st.pyplot(fig=plt1.figure)
+
+
+#  ---- Graduation Performance Coefficient ---- #
+# Coefficient - 1st Period
+P1 = round(history0.loc[history0['Period'] == 1].iloc[:, 8].sum() / history0.loc[history0['Period'] == 1].iloc[:, 5].sum(), 1)
+
+# Coefficient - 2nd Period
+P2 = round(history0.loc[history0['Period'] <= 2].iloc[:, 8].sum() / history0.loc[history0['Period'] <= 2].iloc[:, 5].sum(), 1)
+
+# Coefficient DataSet
+coef0 = [[2022.1, P1],
+       [2022.2, P2]]
+
+# Coefficient DataFrame
+coefficient0 = pd.DataFrame(coef0, columns=['Period', 'Performance Coefficient'])
+
+# Coefficient Histplot
+sb.set_style('whitegrid')
+plt.figure(figsize=(10, 5))
+plt0 = sb.barplot(x='Period', y='Performance Coefficient', data=coefficient0, palette='mako')
+plt0.bar_label(plt0.containers[0])
+plt0.set(yticklabels=[])
+plt0.set_ylabel(None)
+plt0.set_xlabel(None)
+
+# Right Column
+with column5:
+    st.markdown('<h3 style="text-align: center">Graduation Performance Coefficient</h3><br>', unsafe_allow_html=True)
     st.pyplot(fig=plt0.figure)

cbe93f0068565f53553aa381bdefd9a039959bda
Marktheo <marcosvinytheo@gmail.com>
2023-02-27 19:25:59 -0300

    Application Update: Lineplot Axis Configuration

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.set_page_config(page_title='Academic Dashboard', layout='wide')
 
 # ---- Page Header ---- #
 # Column Configuration
-column0, column1 = st.columns([2, 2])
+column0, column1 = st.columns([2.5, 2])
 
 # Left Column
 with column0:
@@ -89,9 +89,10 @@ coefficient = pd.DataFrame(coef, columns=['Period', 'Performance Coefficient'])
 
 # Coefficient Scatterplot
 sb.set_style('whitegrid')
-plt.figure(figsize=(10, 2.75))
+plt.figure(figsize=(10, 3.3))
 plt0 = sb.stripplot(x='Period', y='Performance Coefficient', data=coefficient, palette='mako')
 plt0.set_title('Academic Performance Coefficient')
+plt0.set_ylim(7.8, 9.2)
 plt0.set_ylabel(None)
 plt0.set_xlabel(None)
 
